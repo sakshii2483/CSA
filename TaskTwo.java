7abf63ca03e1249696383514cc0e32faa4a23512
sakshii2483 <sakshii2483@gmail.com>
2025-04-15 16:32:05 +0530

    Add files via upload

--- TaskTwo.java
+++ TaskTwo.java
@@ -10,7 +10,7 @@ public class TaskTwo
     int count = 0;
     for(int i = 0; i < str.length(); i++)
       {
-        if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u')
+        if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I' || str.charAt(i) == 'O' || str.charAt(i) == 'U')
         {
           count++;
         }  
